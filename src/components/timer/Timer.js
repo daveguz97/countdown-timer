a50cfa36bfb9b2c02ff8ae730273c512baabf8ee
David Guzman <davidguzmaninc@gmail.com>
2020-07-19 17:54:54 -0400

    Added my default styling

--- src/components/timer/Timer.js
+++ src/components/timer/Timer.js
@@ -1,17 +1,20 @@
 import React from "react";
+import "./Timer.scss";
 
 export default function Timer() {
   return (
     <div className="Timer">
       <header>
-        <h3>Countdown: </h3>
+        <h2>Countdown: </h2>
         <input type="number" className="timer-input" placeholder="(min)" />
       </header>
       <section>
         <p>More than halfway there!</p>
         <div className="timer-container">
           <h1>15:31</h1>
-          <i class="far fa-pause-circle fa-2x"></i>
+          <button>
+            <i class="far fa-pause-circle fa-2x"></i>
+          </button>
         </div>
         <button>1X</button>
         <button>1.5X</button>
